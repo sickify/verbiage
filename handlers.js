55acd79b55911f7334d60b8c0f206cce76a7a66e
Battenfield <chrisbattenfield@gmail.com>
2016-11-14 06:08:21 -0700

    <sickify>(server): create counter handler using ngram {dope}

--- handlers.js
+++ handlers.js
@@ -1,8 +1,15 @@
 var nlp = require('nlp_compromise');
+var nlpNgram = require('nlp-ngram');
+nlp.plugin(nlpNgram);
 
-const lang = function(text) {
+var lang = function(text) {
   return nlp.sentence(text);
 };
 
+var count = function(text) {
+  var nlpText = nlp.text(text);
+  return nlpText.ngram();
+};
 
 module.exports.lang = lang;
+module.exports.count = count;
