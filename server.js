4309391030b44a8bfe94db40c91ea3324d5fd569
Battenfield <chrisbattenfield@gmail.com>
2016-11-14 06:11:23 -0700

    <sickify>(server): create path for counter object {cool}

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ var handlers = require('./handlers');
 exp.use(express.static('client'));
 
 exp.get('/sentence', function(req, res) {
-  var sen = req.query.input
+  var sen = req.query.input;
   var textData = handlers.lang(sen);
   res.send(textData);
+});
+
+exp.get('/counter', function(req, res) {
+  var sen = req.query.input;
+  var countData = handlers.count(sen);
+  res.send(countData);
 });
 
 exp.listen(1983);
