ff6e55303bfecc85b74887a8568e2fc1ef30193b
Battenfield <chrisbattenfield@gmail.com>
2016-11-22 23:33:50 -0800

    <sickify>(server): create path partsCounter {dope}

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ exp.get('/partsOfSpeech', function(req, res) {
   res.send(partsData);
 });
 
+exp.get('/partsCounter', function(req, res) {
+  var sen = req.query.input;
+  var speechData = handlers.lang(sen).terms;
+  res.send('{ Noun: 5, Verb: 6 }');
+});
+
 exp.get('/counter', function(req, res) {
   var sen = req.query.input;
   var countData = handlers.count(sen)[0];
