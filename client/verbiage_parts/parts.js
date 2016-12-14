eac955f466cdc7343141329e9e0b100aea7b8883
Battenfield <chrisbattenfield@gmail.com>
2016-12-13 20:57:11 -0800

    <sickify>(client): parts controller {sick}

--- client/verbiage_parts/parts.js
+++ client/verbiage_parts/parts.js
@@ -1,11 +1,16 @@
 angular.module('verbiage.parts' [])
   .controller('PartsController', function($scope, $http){
     $scope.parts = {};
-    $http.get('/partsOfSpeech')
-      .then(function(data) {
-        $scope.parts = data;
-      }, function(err) {
-        console.log(err);
-      });
+    
+    var initPaths = function() {
+      Paths.parts()
+        .then(function (data) {
+          $scope.parts = data;
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    };
 
+    initPaths();
   })
