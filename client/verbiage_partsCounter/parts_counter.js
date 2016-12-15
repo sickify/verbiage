576ed2c7325a2fe8acab870ccdb7768643d0c5ac
Battenfield <chrisbattenfield@gmail.com>
2016-12-14 23:59:56 -0800

    <sickify>(client): parts counter {dope}

--- client/verbiage_partsCounter/parts_counter.js
+++ client/verbiage_partsCounter/parts_counter.js
@@ -1,11 +1,13 @@
 angular.module('verbiage.partsCounter' [])
   .controller('PartsCounterController', function($scope, $http){
-    $scope.parts = {};
-    $http.get('/partsCounter')
-      .then(function(data) {
-        $scope.parts = data;
-      }, function(err) {
-        console.log(err);
-      });
-
+    $scope.partsCounter = {};
+    var initPaths = function() {
+      Paths.partsCounter()
+        .then(function (data) {
+          $scope.partsCounter = data;
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    };
   });
