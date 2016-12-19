130f6dbf0b3fe4fdb31830dd30f013b55843ef83
Battenfield <chrisbattenfield@gmail.com>
2016-12-18 22:48:04 -0800

    <sickify>(client): add error handling and logging to factory for development {dev}

--- client/services.js
+++ client/services.js
@@ -3,19 +3,31 @@ angular.module('verbiage')
     var counter = function() {
       return $http.get('/counter')
         .then(function(data) {
+          console.log('from factory ', data);
           return data;
+        })
+        .catch(function(err) {
+          console.log(err);
         });
     };
     var parts = function() {
       return $http.get('/partsOfSpeech')
         .then(function(data) {
+          console.log('from factory ', data);
           return data;
+        })
+        .catch(function(err) {
+          console.log(err);
         });
     };
     var partsCounter = function() {
       return $http.get('/partsCounter')
         .then(function(data) {
+          console.log('from factory ', data);
           return data;
+        })
+        .catch(function(err) {
+          console.log(err);
         });
     };
     return {
