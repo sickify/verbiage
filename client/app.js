ff93eab13f51b56018336d77d65feb8b033e5a57
Battenfield <chrisbattenfield@gmail.com>
2016-12-19 22:18:11 -0800

    <sickify>(client): update app A integration {dope}

--- client/app.js
+++ client/app.js
@@ -1,1 +1,38 @@
-angular.module('verbiage', []);
+angular.module('verbiage', [])
+  .controller('PartsCounterController', function($scope, Paths){
+    $scope.pathsResults = [];
+    $scope.input = "";
+    $scope.parts = function() {
+      Paths.parts()
+        .then(function (data) {
+          $scope.pathsResults = data;
+          console.log($scope.input);
+          console.log(data.data);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    };
+
+    $scope.partsCounter = function() {
+      Paths.partsCounter()
+        .then(function (data) {
+          $scope.pathsResults = data;
+          console.log(data.data);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    };
+
+    $scope.counter = function() {
+      Paths.counter()
+        .then(function (data) {
+          $scope.pathsResults = data;
+          console.log(data.data);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    };
+  });
